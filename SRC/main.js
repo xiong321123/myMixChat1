503732018fd1c21a143cd3e8a4247e3ccb006f15
xiong321123 <549651380@qq.com>
2024-03-07 15:30:51 +0800

    init

--- SRC/main.js
+++ SRC/main.js
@@ -1,21 +1,26 @@
 const { MixinApi } = require('@mixin.dev/mixin-node-sdk');
-
+const keystore=require('./keystore');
 // const keystore = {
 //     app_id: "cbaa6b46-11b3-4bb0-aa77-4a91162683e4",
 //     session_id: "aa6cd5da-9d1c-4839-b1e8-175625c4f6cf",
 //     server_public_key: "9e53982b3c8873be506e1e7431f117cfa91a0db87646a1d79010e1bf61d4338d",
 //     session_private_key: "938215f49a2733b12b6f78f1b592dd3b2d81ea40daff757c368984a13e3a25af",
 //   };
+console.log(keystore);
+
+
+
 
  
-const config=require('./config');
-const keystore = {
-    app_id: config.app_id,
-    session_id: config.session_id,
-    server_public_key: config.server_public_key,
-    session_private_key:config.session_private_key,
-    config,
-  };
+// const config=require('./config');
+
+// const keystore = {
+//     app_id: config.app_id,
+//     session_id: config.session_id,
+//     server_public_key: config.server_public_key,
+//     session_private_key:config.session_private_key,
+//     config,
+//   };
 
 
 
@@ -107,7 +112,7 @@ async function zuoye3(data){
 
 }
 
-zuoye3(['41102708','7000104824','7000104341','7000102069','7000105018'])
+// zuoye3(['41102708','7000104824','7000104341','7000102069','7000105018'])
 
 
 // async function zuoye9(){
